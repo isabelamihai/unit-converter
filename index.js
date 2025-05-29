3cbfc6c1da5fee6ea668016f8e457f2eedf031f8
isabelamihai <isabelaelenamihai@gmail.com>
2025-05-29 21:59:24 +0300

    Update

--- index.js
+++ index.js
@@ -9,16 +9,35 @@ const convertBtn = document.getElementById("convert-btn");
 const convertL = document.getElementById("l-convert");
 const convertV = document.getElementById("v-convert");
 const convertM = document.getElementById("m-convert");
+const themeBtn =  document.getElementById("theme-btn")
+const listEl = document.querySelectorAll("li");
+const textP = document.querySelectorAll("p");
+const textH = document.querySelectorAll("li>h2");
 
-
-convertBtn.addEventListener("click", function(){
+convertBtn.addEventListener("click", function () {
    let measure = Number(inputBox.value);
-   let length = `${measure} meters = ${(measure*3.281).toFixed(3)} feet | ${measure} feet = ${(measure*0.304).toFixed(3)} meters`;
+   let length = `${measure} meters = ${(measure * 3.281).toFixed(3)} feet | ${measure} feet = ${(measure * 0.304).toFixed(3)} meters`;
    convertL.textContent = length;
    
-   let volume = `${measure} liters = ${(measure*0.264).toFixed(3)} gallons | ${measure} gallons = ${(measure*3.787).toFixed(3)} liters`;
+   let volume = `${measure} liters = ${(measure * 0.264).toFixed(3)} gallons | ${measure} gallons = ${(measure * 3.787).toFixed(3)} liters`;
    convertV.textContent = volume;
    
-    let mass = `${measure} kilos = ${(measure*2.204).toFixed(3)} pounds | ${measure} pounds = ${(measure*0.453).toFixed(3)} kilos`;
+   let mass = `${measure} kilos = ${(measure * 2.204).toFixed(3)} pounds | ${measure} pounds = ${(measure * 0.453).toFixed(3)} kilos`;
    convertM.textContent = mass;
-})
+});
+
+   themeBtn.addEventListener("click", function () {
+   document.getElementById("container-body").style.backgroundColor = '#1F2937';
+      
+   listEl.forEach(item => {
+  item.style.backgroundColor = '#273549';
+   }); 
+      
+      textP.forEach(itemP => {
+         itemP.style.color = '#FFFFFF';
+      });
+   
+      textH.forEach(itemH => {
+         itemH.style.color = '#CCC1FF';
+      })
+   })
